33822a89fe7e8333df6cf06c0690fa2059859d5a
Artem Novikov <artem.247@gmail.com>
2020-04-27 07:54:17 +0300

    log out, log in functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url
+      log_in @user
+      redirect_to @user
     else
       render 'new'
     end
   end
+
+
 
   private
     
