f81f78af18329b444662187fdb28545d1aa00cbd
Artem Novikov <artem.247@gmail.com>
2020-04-27 12:31:02 +0300

    fixes

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,6 +1,6 @@
 class TodoListsController < ApplicationController
   before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user, only: [:create, :destroy, :new]
 
   # GET /todo_lists
   # GET /todo_lists.json
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :new]
   def new
     @user = User.new
   end
